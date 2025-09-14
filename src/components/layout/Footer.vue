6bfd7a204c982525b702fa3b7e9178ec10d3b94f
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-09-14 17:13:13 -0400

    finished updating all expertise and skills

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,12 +2,15 @@
 	<footer class="footer">
 		<div class="content has-text-centered">
 			<p>
-				<strong>Project New Moon Calculators</strong> by
+				<strong>Project New Moon Calculators</strong>, originally made by
 				<a href="https://github.com/kuenaimaku">Kuenaimaku</a>. The <a href="https://github.com/Kuenaimaku/new-moon-tools">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.<br/>
 				I am not affiliated with the Project New Moon team. I just want to make planning for this game simpler.
 			</p>
 			<p>
-				Expertise calculator updated by <a href="https://github.com/IuriTeixeira">Glorienn</a>.
+				Expertise calculator updated by <a href="https://github.com/IuriTeixeira">Glorienn</a>, and <a href="https://github.com/IuriTeixeira/new-moon-tools">the new source code is here</a>.
+			</p>
+			<p>
+				Be sure to also check out my <a href="https://new-moon-fusion-tool.vercel.app/">Fusion Tool</a>!
 			</p>
 		</div>
 	</footer>
